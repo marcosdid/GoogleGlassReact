7ae756f6ab9dfae558c97995a5791ce603674c11
marcosdid <limafilhoguerreiro@outlook.com>
2020-08-06 00:33:08 -0300

    Updates

---
+++ precache-manifest.0352450d137b5350721f2d3a2ea0a432.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "220d3996f876c94c0e3c5355eeb51857",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3192357a05dec96adeaa",
+    "url": "/static/css/2.05a0ae32.chunk.css"
+  },
+  {
+    "revision": "3192357a05dec96adeaa",
+    "url": "/static/js/2.446c4cf5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.446c4cf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33fc1d3482f6a8408e68",
+    "url": "/static/js/main.e2447fb0.chunk.js"
+  },
+  {
+    "revision": "2e7e11ecbbad63bf5ff9",
+    "url": "/static/js/runtime-main.d25277ad.js"
+  },
+  {
+    "revision": "7e433573314a55cef042ce0a970de234",
+    "url": "/static/media/album1.7e433573.jpg"
+  },
+  {
+    "revision": "62f77cc6d7478430adb3fe70b1d1b50a",
+    "url": "/static/media/album11.62f77cc6.jpg"
+  },
+  {
+    "revision": "4a0e97814edb23f74dff51576bdd7d23",
+    "url": "/static/media/album2.4a0e9781.jpg"
+  },
+  {
+    "revision": "7cf40b6d51a6188d9812f97eafac59da",
+    "url": "/static/media/album3.7cf40b6d.jpg"
+  },
+  {
+    "revision": "bdc1bc1a0e83aa97ed899b58706e1bd3",
+    "url": "/static/media/album4.bdc1bc1a.jpg"
+  },
+  {
+    "revision": "c125f09239bad70ae62c98da98d95af3",
+    "url": "/static/media/como-google-glass-funciona.c125f092.jpg"
+  },
+  {
+    "revision": "b72522d758482feab9dd83786fffb942",
+    "url": "/static/media/glass-esquema-marcado.b72522d7.jpg"
+  },
+  {
+    "revision": "2a829d85f2832c966c42b788d78d5c66",
+    "url": "/static/media/google-glass-model.2a829d85.jpg"
+  },
+  {
+    "revision": "f19612f6171a43e9b5ea2bd65daa9212",
+    "url": "/static/media/pngegg.f19612f6.png"
+  }
+]);
