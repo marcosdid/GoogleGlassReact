e1229ab599483a72119cff9446f67151011faf14
marcosdid <limafilhoguerreiro@outlook.com>
2020-08-06 01:02:04 -0300

    Updates

---
+++ precache-manifest.9c7464e414f36d9e5c50aae89ba5162f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fff60d67d377b68c50d37defee781e75",
+    "url": "/GoogleGlassReact/index.html"
+  },
+  {
+    "revision": "3759d13ad9d28ecdb55f",
+    "url": "/GoogleGlassReact/static/css/2.05a0ae32.chunk.css"
+  },
+  {
+    "revision": "3759d13ad9d28ecdb55f",
+    "url": "/GoogleGlassReact/static/js/2.75008936.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/GoogleGlassReact/static/js/2.75008936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e9ebd95297e474146fb",
+    "url": "/GoogleGlassReact/static/js/main.2fac0460.chunk.js"
+  },
+  {
+    "revision": "a5b0b701385260cab66d",
+    "url": "/GoogleGlassReact/static/js/runtime-main.ff3d592a.js"
+  },
+  {
+    "revision": "7e433573314a55cef042ce0a970de234",
+    "url": "/GoogleGlassReact/static/media/album1.7e433573.jpg"
+  },
+  {
+    "revision": "62f77cc6d7478430adb3fe70b1d1b50a",
+    "url": "/GoogleGlassReact/static/media/album11.62f77cc6.jpg"
+  },
+  {
+    "revision": "4a0e97814edb23f74dff51576bdd7d23",
+    "url": "/GoogleGlassReact/static/media/album2.4a0e9781.jpg"
+  },
+  {
+    "revision": "7cf40b6d51a6188d9812f97eafac59da",
+    "url": "/GoogleGlassReact/static/media/album3.7cf40b6d.jpg"
+  },
+  {
+    "revision": "bdc1bc1a0e83aa97ed899b58706e1bd3",
+    "url": "/GoogleGlassReact/static/media/album4.bdc1bc1a.jpg"
+  },
+  {
+    "revision": "c125f09239bad70ae62c98da98d95af3",
+    "url": "/GoogleGlassReact/static/media/como-google-glass-funciona.c125f092.jpg"
+  },
+  {
+    "revision": "b72522d758482feab9dd83786fffb942",
+    "url": "/GoogleGlassReact/static/media/glass-esquema-marcado.b72522d7.jpg"
+  },
+  {
+    "revision": "2a829d85f2832c966c42b788d78d5c66",
+    "url": "/GoogleGlassReact/static/media/google-glass-model.2a829d85.jpg"
+  },
+  {
+    "revision": "f19612f6171a43e9b5ea2bd65daa9212",
+    "url": "/GoogleGlassReact/static/media/pngegg.f19612f6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.39e43073e06ad402fe054d47352460bc.js"
+  "/GoogleGlassReact/precache-manifest.9c7464e414f36d9e5c50aae89ba5162f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GoogleGlassReact/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
